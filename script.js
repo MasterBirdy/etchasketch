2e174a4d4b8b634f3b4ef6fd05d38b50b1bae07b
Matthew Ito <mito81@gmail.com>
2019-11-28 16:30:23 -0800

    started and made grid

---
+++ script.js
@@ -0,0 +1,17 @@
+function fillBoard(){
+     for(let i = 0; i < 16; i++)
+     {
+          let row = document.createElement("div");
+          row.classList.add("row");
+          for(let j = 0; j < 16; j++)
+          {
+               let block = document.createElement("div");
+               block.classList.add("block");
+               row.appendChild(block);
+          }
+          container.appendChild(row);
+     } 
+}
+
+const container = document.getElementById("container");
+fillBoard();
